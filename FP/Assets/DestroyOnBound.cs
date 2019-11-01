feed160944b92323f8d679f411400eb5952d1367
minhtam910 <56921728+minhtam910@users.noreply.github.com>
2019-11-01 08:09:09 +0700

    spawn được rồi

---
+++ FP/Assets/DestroyOnBound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnBound : MonoBehaviour {
+
+    private Vector2 destroyBound;
+	// Use this for initialization
+	void Start () {
+        destroyBound = new Vector2((float)5, (float) -1.06);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x > destroyBound.x + 0.01)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ FP/Assets/ZombieSpawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawn : MonoBehaviour {
+
+    public GameObject maleZom;
+    public GameObject femaleZom;
+    private Vector2 screenBounds;
+    private float spawnTime = 3.0f;
+    private float n;
+    // Use this for initialization
+    void Start () {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(zombieWave());
+    }
+	private void SpawnEnemy(float n)
+    {
+        if(n >= 1)
+        {
+            GameObject a = Instantiate(maleZom) as GameObject;
+            a.transform.position = new Vector2(-screenBounds.x +2 , Random.Range((float)-3.93, (float)-2.31));
+        }
+        else
+        {
+            GameObject a = Instantiate(femaleZom) as GameObject;
+            a.transform.position = new Vector2(-screenBounds.x +2 , Random.Range((float)-3.93, (float)-2.31));
+        }
+    }
+	// Update is called once per frame
+	IEnumerator zombieWave()
+    {
+        while(true)
+        {
+            n = Random.Range(0, 2);
+            yield return new WaitForSeconds(spawnTime);
+            SpawnEnemy(n);
+        }
+    }
+}
